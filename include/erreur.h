99b0869c136555a55b1b9d842c0ec0eff84a6a93
jashbin <jasmin.galbrun@gmail.com>
2019-03-05 11:30:46 +0100

    ADD erreur.c + erreur.h

---
+++ include/erreur.h
@@ -0,0 +1,18 @@
+/**
+ * \file erreur.h
+ * \author Jasmin GALBRUN
+ * \version 1
+ * \date 05/03/2019
+ * \brief Module de gestion des erreurs
+ * 
+ * \enum t_erreur
+ * \brief Contient tout les codes erreurs
+ */
+typedef enum e_erreur{
+    OK,
+    FILE_ERROR,
+    OPEN_FILE_ERROR,
+    CLOSE_FILE_ERROR,
+    VALUE_ERROR,
+    ERROR_NOT_EXIST
+}t_erreur;
---
+++ src/erreur.c
@@ -0,0 +1,54 @@
+/**
+ * \file erreur.c
+ * \author Jasmin GALBRUN
+ * \version 1
+ * \date 05/03/2019
+ * \brief Module de gestion des erreurs
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <erreur.h>
+
+#define NB_ERREUR 5
+
+/**
+ * \struct t_erreur_s
+ * \brief Contient l'id du code erreur + le message correspondant à l'erreur
+*/
+typedef struct s_erreur
+{
+    t_erreur id;
+    char * msg;
+} t_erreur_s;
+
+//Tableau contenant toutes erreurs + les messages correspondants
+t_erreur_s tab_err = {
+    {OK, "Tout c est bien passer"},
+    {FILE_ERROR, "Erreur de fichier"},
+    {OPEN_FILE_ERROR, "Erreur d ouverture du fichier"},
+    {CLOSE_FILE_ERROR, "Erreur de fermeture du fichier"},
+    {VALUE_ERROR, "Erreur de valeur"},
+    {ERROR_NOT_EXIST, "Cette erreur n existe pas"}
+}
+
+/**
+ * \fn t_erreur aff_erreur(t_erreur code_erreur)
+ * \param code_erreur Code erreur que le programme doit afficher
+ * \return Retourne une erreur, OK si tout c'est bien passé
+*/
+t_erreur aff_erreur(t_erreur code_erreur){
+    int i;
+    for(i = 0; i < NB_ERREUR; i++){
+        if(tab_err[i].id == code_erreur){
+            break;
+        }
+    }
+
+    if(i >= NB_ERREUR){
+        fprintf(stderr, "Erreur : Code erreur %d non trouve\n", code_erreur);
+        return ERROR_NOT_EXIST;
+    }
+
+    fprintf(stderr, "Erreur : [%d] - %s\n", tab_err[i].id, tab_err[i].msg);
+    return OK;
+}
