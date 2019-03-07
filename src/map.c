a8b3ab304e72c4c54d3765d79fcf454d311b24cb
Checkam <maxencedespres@gmail.com>
2019-03-07 20:03:31 +0100

    Création des fonctions Charger, Sauvegarde, Destruction

--- src/map.c
+++ src/map.c
@@ -23,54 +23,106 @@
 
 
 /**
-    \fn t_map * MAP_creer(char * nom_map, int SEED)
-    \brief Créer une map
+    \fn t_erreur MAP_creer(t_map ** map, char * nom_map, int SEED) 
+    \brief Créer une map (le dossier de saugarde et le pointeur pour manipuler cette map)
+    \param map le pointeur de la map qui va être créer
     \param nom_map Le nom de la map
     \param SEED Le SEED de génération de la map
-    \return renvoie le pointeur correspondant
+    \return Renvoie un code erreur en cas de problème sinon OK
 **/
-t_map * MAP_creer(char * nom_map, int SEED) {
+t_erreur MAP_creer(t_map ** map, char * nom_map, int SEED) {
 
-    t_map *map  = malloc(sizeof(t_map));
-    map->nom = malloc(sizeof(char) * strlen(nom_map) + 1);
-    strcpy(map->nom,nom_map);
-    map->SEED = SEED;
+    (*map) = malloc(sizeof(t_map));
+    (*map)->nom = malloc(sizeof(char) * strlen(nom_map) + 1);
 
-    MAP_creer_dir(map);
+    strcpy((*map)->nom,nom_map);
+    (*map)->SEED = SEED;
 
-    return map;
+    MAP_creer_dir(*map);
+
+    MAP_sauvegarder(*map);
+
+    return OK;
+}
+
+t_erreur MAP_charger(t_map ** map, char * nom_map){
+    (*map)  = malloc(sizeof(t_map));
+    char * path_dir = MAP_creer_path(*map);
+    char * curr_dir;
+    getcwd(curr_dir,500);
+    if(chdir(path_dir)) return FILE_ERROR; //DIR_NO_FOUND
+    chdir(curr_dir);
+
+    (*map)->nom = malloc(sizeof(char) * strlen(nom_map) + 1);
+    strcpy((*map)->nom,nom_map);
+
+    //Charger le seed avec fct json
+
+    MAP_detruire_path(&path_dir); //Gestion des erreurs a faire
+    return OK;
+}
+
+t_erreur MAP_lister(){
+    
+    return OK;
+}
+
+
+/**
+    \fn t_erreur MAP_sauvegarder(t_map * map)
+    \brief Sauvegarde la map dans l'état actuel 
+    \param map Pointeur sur la map qui doit etre sauvegarder
+    \return Renvoie un code erreur en cas de problème sinon OK
+**/
+t_erreur MAP_sauvegarder(t_map * map){
+    if(map == NULL) return PTR_VALUE_ERROR; //PTR_NULL
+
+    char * path_dir = MAP_creer_path(map);
+
+    //A finir avec le systeme de JSON est de path pas encore disponible
+    //Sauvegarde de ou est le joueurs pour preload la map a afficher
+
+    MAP_detruire_path(&path_dir); //Gestion des erreurs a faire 
+
+    return OK;
 }
 
 /**
     \fn t_erreur MAP_creer_dir(t_map * map)
     \brief Créer le repertoire d'une map
     \param map Pointeur sur la map dont le repertoire doit être créer 
-    \return renvoie un code erreur (OK,POINTEUR_NULL)
+    \return Renvoie un code erreur en cas de problème sinon OK
 **/
 t_erreur MAP_creer_dir(t_map * map) {
-    if(map == NULL) return PTR_NULL;
+    if(map == NULL) return PTR_VALUE_ERROR; //PTR_NULL
+    t_erreur erreur;
+    char * path_dir = MAP_creer_path(map);
 
-    char * path_dir = malloc(sizeof(char) * strlen(map->nom) + sizeof(char) * strlen(PATH_MAP_DIR));
-    strcpy(path_dir,PATH_MAP_DIR);
-    strcat(path_dir,map->nom);
-    mkdir(path_dir,755);
+    if(mkdir(path_dir,755))
+        return FILE_ERROR; //MKDIR_ERROR
+    
+    erreur = MAP_detruire_path(&path_dir);
+    if(erreur != OK)
+        return erreur;
 
     return OK;
+
 }
 
 /**
     \fn t_erreur MAP_supprimer_dir(t_map * map)
     \brief Supprimer le repertoire d'une map
     \param map Pointeur sur la map dont le repertoire doit être supprimer 
-    \return renvoie le pointeur correspondant
+    \return Renvoie un code erreur en cas de problème sinon OK
 **/
-t_erreur MAP_supprimer_dir(t_map * map) {
-    if(map == NULL) return PTR_NULL;
+t_erreur MAP_supprimer(t_map * map) {
+    if(map == NULL) return PTR_VALUE_ERROR; //PTR_NULL
 
-    char * path_dir = malloc(sizeof(char) * strlen(map->nom) + sizeof(char) * strlen(PATH_MAP_DIR));
-    strcpy(path_dir,PATH_MAP_DIR);
-    strcat(path_dir,map->nom);
+    char * path_dir = MAP_creer_path(map);
+
     rmdir(path_dir);
+
+    MAP_detruire_path(&path_dir);
 
     return OK;
 }
@@ -79,10 +131,10 @@ t_erreur MAP_supprimer_dir(t_map * map) {
     \fn t_erreur MAP_supprimer(t_map ** map)
     \brief Supprime un pointeur t_map
     \param map Pointeur de pointeur de t_map à supprimer et mettre a NULL
-    \return renvoie le pointeur correspondant
+    \return Renvoie un code erreur en cas de problème sinon OK
 **/
-t_erreur MAP_supprimer(t_map ** map) {
-    if(map == NULL) return PTR_NULL;
+t_erreur MAP_detruction(t_map ** map) {
+    if(map == NULL) return PTR_VALUE_ERROR; //PTR_NULL
 
     if((*map)->nom != NULL){
         free((*map)->nom);
@@ -93,4 +145,31 @@ t_erreur MAP_supprimer(t_map ** map) {
     return OK;
 }
 
+/**
+    \fn char * MAP_path(t_map * map)
+    \brief Créer le path d'une map
+    \param map Pointeur de t_map
+    \return Renvoie le path de la map
+**/
+char * MAP_creer_path(t_map * map){
+    char * path_dir = malloc(sizeof(char) * strlen(map->nom) + sizeof(char) * strlen(PATH_MAP_DIR));
+    strcpy(path_dir,PATH_MAP_DIR);
+    strcat(path_dir,map->nom);
+    return path_dir;
+}
+
+/**
+    \fn t_erreur MAP_detruire_path(char ** path_dir)
+    \brief Libère la mémoire dynamique alloué pour le path 
+    \param map Pointeur de pointeur sur le path
+    \return Renvoie un code erreur en cas de problème sinon OK
+**/
+t_erreur MAP_detruire_path(char ** path_dir){
+    if(*path_dir == NULL) return PTR_VALUE_ERROR; //PTR_NULL
+
+    free(*path_dir);
+    path_dir = NULL;
+    return OK;
+
+}
 
