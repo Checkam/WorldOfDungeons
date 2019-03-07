6a40ccb2ad1c6bdfc8e80ab01ddf862cd15654a5
Checkam <maxencedespres@gmail.com>
2019-03-07 17:48:48 +0100

    Création et modification de fonction du module map - Création MAP_creer_dir - Modification MAP_creer

--- include/map.h
+++ include/map.h
@@ -1,4 +1,12 @@
 
+#ifndef __MAP_H__
+#define __MAP_H__
+
+#include <erreur.h>
+#endif
+
+#define PATH_MAP_DIR "map/"
+
 /**
  \struct t_map
  \brief Structure map
@@ -10,5 +18,7 @@ typedef struct s_map {
 } t_map; 
 
 t_map * MAP_creer(char * nom_map, int SEED);
-t_erreur MAP_supprimer_dir(t_map * map);
 t_erreur MAP_supprimer(t_map ** map);
+
+t_erreur MAP_creer_dir(t_map * map);
+t_erreur MAP_supprimer_dir(t_map * map);
