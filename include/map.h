7fb1c01a7d549b9d5acee3717f56331e8b8a375e
Checkam <maxencedespres@gmail.com>
2019-03-07 20:03:31 +0100

    Création des fonctions Charger, Sauvegarde, Destruction

--- include/map.h
+++ include/map.h
@@ -17,8 +17,14 @@ typedef struct s_map {
     // Une liste
 } t_map; 
 
-t_map * MAP_creer(char * nom_map, int SEED);
-t_erreur MAP_supprimer(t_map ** map);
+t_erreur MAP_creer(t_map ** map, char * nom_map, int SEED) ;
+t_erreur MAP_charger(t_map ** map, char * nom_map);
+t_erreur MAP_detruction(t_map ** map);
 
 t_erreur MAP_creer_dir(t_map * map);
-t_erreur MAP_supprimer_dir(t_map * map);
+t_erreur MAP_supprimer(t_map * map);
+
+t_erreur MAP_sauvegarder(t_map * map);
+
+char * MAP_creer_path(t_map * map);
+t_erreur MAP_detruire_path(char ** path_dir);
