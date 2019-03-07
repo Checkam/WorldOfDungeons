7c5667f6fa2a32afef69ee4ff0f607bff143ce63
jashbin <jasmin.galbrun@gmail.com>
2019-03-07 12:15:51 +0100

    Modification Module menu menu.h : - ajout structure + prototype des fonctions de création d'un menu
    menu.c :
    - amélioration des fonctions

--- src/menu.c
+++ src/menu.c
@@ -9,37 +9,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <SDL2/SDL.h>
 #include <erreur.h>
 #include <menu.h>
 
-/**
- * \struct t_bouton_menu
- * \brief Contient les informations d'un bouton
-*/
-typedef struct s_bouton_menu{
-    Uint16_t x, y, width, height;
-    char * titre;
-    SDL_Texture * texture;
-} t_bouton_menu;
 
 /**
- * \struct t_menu
- * \brief Contient l'ensemble des boutons appartenant au menu
-*/
-typedef struct s_menu{
-    t_bouton_menu * tab_bouton;
-    Uint8_t nb_bouton;
-}t_menu;
-
-
-/**
- * \fn t_erreur ajout_bouton_menu(t_menu * menu, Uint16_t x, Uint16_t y, Uint16_t width, Uint16_t height, char * titre)
+ * \fn t_erreur ajout_bouton_menu(t_menu * menu, uint16_t x, uint16_t y, uint16_t width, uint16_t height, char * titre, SDL_Texture * texture)
  * \param 
 */
-t_erreur ajout_bouton_menu(t_menu * menu, Uint16_t x, Uint16_t y, Uint16_t width, Uint16_t height, char * titre, SDL_Texture * texture){
+t_erreur ajout_bouton_menu(t_menu * menu, uint16_t x, uint16_t y, uint16_t width, uint16_t height, char * titre, SDL_Texture * texture){
     if(menu == NULL){
         return UNDEFINED_MENU;
+    }
+
+    /* On ajoute un bouton au menu */
+    if(menu->tab_bouton == NULL){
+        menu->tab_bouton = malloc(sizeof(t_bouton_menu));
+    }else{
+        menu->tab_bouton = realloc(menu->tab_bouton, (menu->nb_bouton + 1) * sizeof(t_bouton_menu));
     }
 
     t_bouton_menu * btn = malloc(sizeof(t_bouton_menu));
@@ -55,6 +44,7 @@ t_erreur ajout_bouton_menu(t_menu * menu, Uint16_t x, Uint16_t y, Uint16_t width
 
     if(texture == NULL){
         //valeur par défaut de la texture d'un bouton
+        btn->texture = NULL;
     }else{
         btn->texture = texture;
     }
@@ -66,33 +56,35 @@ t_erreur ajout_bouton_menu(t_menu * menu, Uint16_t x, Uint16_t y, Uint16_t width
 }
 
 /**
- * \fn t_erreur creer_menu(t_type_menu type, Uint16_t width, Uint16_t height, t_menu * menu)
+ * \fn t_erreur creer_menu(t_type_menu type, uint16_t width, uint16_t height, t_menu * menu)
  * \param 
 */
-t_erreur creer_menu(t_type_menu type, Uint16_t width, Uint16_t height, t_menu * menu){
+t_erreur creer_menu(t_type_menu type, uint16_t width, uint16_t height, t_menu * menu){
     int w, h; //Taille d'une colonne et d'une ligne
 
     menu = malloc(sizeof(t_menu));
-    menu->nb_bouton = 0;
+    menu->nb_bouton = 1;
 
     if(type == PRINCIPAL){
-        menu->tab_bouton = malloc(sizeof(t_bouton_menu) * 4); //4 correspond au nombre de boutons dans le menu principal
+        menu->tab_bouton = malloc(sizeof(t_bouton_menu));
 
         w = width / 3;
         h = height / 15;
-        ajout_bouton_menu(w, 5 * h, w, 2 * h, "Solo", NULL);
-        ajout_bouton_menu(w, 7 * h, w, 2 * h, "Multijoueur", NULL);
-        ajout_bouton_menu(w, 9 * h, w, 2 * h, "Option", NULL);
-        ajout_bouton_menu(w, 11 * h, w, 2 * h, "Quitter", NULL);
+        ajout_bouton_menu(w, 5 * h, w, 2 * h, "Solo", NULL, menu);
+        ajout_bouton_menu(w, 7 * h, w, 2 * h, "Multijoueur", NULL, menu);
+        ajout_bouton_menu(w, 9 * h, w, 2 * h, "Option", NULL, menu);
+        ajout_bouton_menu(w, 11 * h, w, 2 * h, "Quitter", NULL, menu);
     }else if(type == SOLO){
-        menu->tab_bouton = malloc(sizeof(t_bouton_menu) * 3);
+        menu->tab_bouton = malloc(sizeof(t_bouton_menu));
 
         w = width / 3;
-        h = height / 14;
-        ajout_bouton_menu(w, , w, 2 * h, "Nouvelle partie", NULL);
-        ajout_bouton_menu(w, , w, 2 * h, "charger une partie", NULL);
-        ajout_bouton_menu(w, , w, 2 * h, "Retour", NULL);
+        h = height / 15;
+        ajout_bouton_menu(w, 6 * h, w, 2 * h, "Nouvelle partie", NULL, menu);
+        ajout_bouton_menu(w, 8 * h, w, 2 * h, "charger une partie", NULL, menu);
+        ajout_bouton_menu(w, 10 * h, w, 2 * h, "Retour", NULL, menu);
     }else{
         menu->tab_bouton = NULL;
     }
+
+    return OK;
 }
