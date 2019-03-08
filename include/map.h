1d5a04a758ac522f26447b44373a208b206253f6
Checkam <maxencedespres@gmail.com>
2019-03-08 10:14:36 +0100

    Modification MAP_creer_path

--- include/map.h
+++ include/map.h
@@ -15,7 +15,7 @@ typedef struct s_map {
     char * nom;
     int SEED;
     // Une liste
-} t_map; 
+} t_map;
 
 t_erreur MAP_creer(t_map ** map, char * nom_map, int SEED) ;
 t_erreur MAP_charger(t_map ** map, char * nom_map);
@@ -26,5 +26,5 @@ t_erreur MAP_supprimer(t_map * map);
 
 t_erreur MAP_sauvegarder(t_map * map);
 
-char * MAP_creer_path(t_map * map);
+char * MAP_creer_path(char * nom_map);
 t_erreur MAP_detruire_path(char ** path_dir);
