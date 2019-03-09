2641eb06cad7026a11a56e97146f07f48cf870d6
jashbin <jasmin.galbrun@gmail.com>
2019-03-09 15:40:20 +0100

    Ajout module outils outils.h : - Ajout des primitives de fonction pour créer des textures - Ajout enum t_type_dessin_texte
    outils.c :
    - Ajout des fonctions de création de texture

---
+++ include/outils.h
@@ -0,0 +1,23 @@
+/**
+ * \file outils.h
+ * \brief Contient une boite à outils de primitives de fonctions utiles
+ * \author Jasmin GALBRUN
+ * \date 09/03/2019
+ * \version 1
+*/
+#ifndef __OUTILS_H__
+#define __OUTILS_H__
+
+#include <SDL2/SDL.h>
+#include <erreur.h>
+
+typedef enum e_type_dessin_texte{
+    SOLID,
+    SHADED,
+    BLENDED
+}t_type_dessin_texte;
+
+t_erreur Create_IMG_Texture(SDL_Renderer * renderer, char * chemin_img, SDL_Texture * *texture_dest);
+t_erreur Create_Text_Texture(SDL_Renderer * renderer, char * texte, char * chemin_police, int taille_police, SDL_Color couleur, t_type_dessin_texte type, SDL_Texture * *texture_dest);
+
+#endif
---
+++ src/outils.c
@@ -0,0 +1,99 @@
+/**
+ * \file outils.c
+ * \brief Contient une boite à outils de fonctions utiles
+ * \author Jasmin GALBRUN
+ * \date 09/03/2019
+ * \version 1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <erreur.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <outils.h>
+
+/**
+ * \fn t_erreur Create_IMG_Texture(SDL_Renderer * renderer, char * chemin_img, SDL_Texture * *texture_dest)
+ * \brief Créé la texture correspondant à l'image passé en paramètre
+ * \param renderer
+ * \param chemin_img chemin de l'image
+ * \param texture_dest Texture de destination de l'image
+ * \return Code erreur
+*/
+t_erreur Create_IMG_Texture(SDL_Renderer * renderer, char * chemin_img, SDL_Texture * *texture_dest){
+    /* Vérification */
+    if(renderer == NULL){
+        return PTR_NULL;
+    }
+    if(texture_dest == NULL){
+        return PTR_NULL;
+    }
+
+    /* Création Surface */
+    SDL_Surface * image = IMG_Load(chemin_img);
+    if(image == NULL){
+        return PTR_NULL;
+    }
+
+    /* création Texture */
+    *texture_dest = SDL_CreateTextureFromSurface(renderer, image);
+    SDL_FreeSurface(image);
+
+    if(*texture_dest == NULL){
+        return NULL;
+    }
+    return OK;
+}
+
+/**
+ * \fn t_erreur Create_Text_Texture(SDL_Renderer * renderer, char * texte, char * chemin_police, int taille_police, SDL_Color couleur, t_type_dessin_texte type, SDL_Texture * *texture_dest)
+ * \brief Créé la texture correspondant au texte passé en paramètre
+ * \param renderer
+ * \param texte texte que l'on veut afficher
+ * \param chemin_police police de caractère du texte
+ * \param taille_police taille de la police
+ * \param couleur couleur du texte
+ * \param type type d'affichage du texte
+ * \param texture_dest Texture de destination du texte
+ * \return Code erreur 
+*/
+t_erreur Create_Text_Texture(SDL_Renderer * renderer, char * texte, char * chemin_police, int taille_police, SDL_Color couleur, t_type_dessin_texte type, SDL_Texture * *texture_dest){
+    /* Vérification */
+    if(renderer == NULL){
+        return PTR_NULL;
+    }
+    if(texture_dest == NULL){
+        return PTR_NULL;
+    }
+
+    /* Création police */
+    TTF_Font *police = TTF_OpenFont(chemin_police, taille_police);
+    if(police == NULL){
+        return PTR_NULL;
+    }
+
+    /* Création Surface */
+    SDL_Surface * texte_surface = NULL;
+    if(type == BLENDED){
+        texte_surface = TTF_RenderText_Blended(police, texte, couleur);
+    }else{
+        return TYPE_ERROR;
+    }
+    if(texte_surface == NULL){
+        return PTR_NULL;
+    }
+
+    /* Création Texture */
+    *texture_dest = SDL_CreateTextureFromSurface(renderer, texte_surface);
+    if(*texture_dest == NULL){
+        return PTR_NULL;
+    }
+
+    /* Destruction pointeur */
+    SDL_FreeSurface(texte_surface);
+    TTF_CloseFont(police);
+
+    return OK;
+}
