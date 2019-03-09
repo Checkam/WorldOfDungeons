697c61551f5ff16c40ba6f94ba6a4b584fdc405f
Checkam <maxencedespres@gmail.com>
2019-03-09 17:40:32 +0100

    Modification du test_map pour test la liste de la map

--- test/MAP/test_map.c
+++ test/MAP/test_map.c
@@ -1,22 +1,51 @@
+#include <affichage.h>
+#include <block.h>
+#include <couleurs.h>
+#include <erreur.h>
+#include <generation.h>
+#include <liste.h>
+#include <map.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <map.h>
 
 int main(int argc, char const *argv[]) {
-  t_map * map = NULL;
-  printf("Création de la map et du repertoire : -- %s\n",MAP_creer(&map,"World 1",121212) == OK? "OK" : "NOK");
+  t_map *map = NULL;
+  int *tab;
+  int boucle = 0;
+  block_type2_t blocks2[NB_BLOCK] = {{AIR, "air", NOIR},      {HERBE, "herbe", VERT},     {TERRE, "terre", MARRON}, {EAU, "eau", BLEU},
+                                     {SABLE, "sable", JAUNE}, {FEUILLE, "feuille", VERT}, {BOIS, "bois", MARRON},   {ROCHE, "roche", GRIS},
+                                     {NEIGE, "neige", BLANC}, {GLACE, "glace", CYAN}};
 
-  printf("Nom map : %s avec comme SEED : %d\n",map->nom,map->SEED);
+  printf("Création de la map et du repertoire : --");
+  aff_erreur(MAP_creer(&map, "World", 121212));
+  printf("\n");
 
-  printf("Suppression d'une map : -- %s\n",MAP_supprimer(map) == OK? "OK" : "NOK");
+  printf("Nom map : %s avec comme SEED : %d\n", map->nom, map->SEED);
 
-  printf("Destruction de la zone mémoire pointer par map : -- %s\n",MAP_detruction(&map) == OK? "OK" : "NOK");
+  while (boucle++ <= 50) {
+    gen_col(&tab, boucle);
+    en_queue(map->list);
+    ajout_droit(map->list, tab);
+  }
 
-  printf("Chargement d'une map : -- %s\n",MAP_charger(&map,"Coucou") == OK? "OK" : "NOK");
+  aff_map(map->list, 30, 80, blocks2);
 
-  printf("Nom map : %s avec comme SEED : %d\n",map->nom,map->SEED);
+  printf("Destruction de la zone mémoire pointer par map : -- ");
+  aff_erreur(MAP_detruction(&map));
+  printf("\n");
 
-  printf("Destruction de la zone mémoire pointer par map : -- %s\n",MAP_detruction(&map) == OK? "OK" : "NOK");
+  printf("Chargement d'une map : -- ");
+  aff_erreur(MAP_charger(&map, "World"));
+  printf("\n");
 
+  printf("Nom map : %s avec comme SEED : %d\n", map->nom, map->SEED);
+
+  printf("Suppression d'une map : -- ");
+  aff_erreur(MAP_supprimer(map));
+  printf("\n");
+
+  printf("Destruction de la zone mémoire pointer par map : -- ");
+  aff_erreur(MAP_detruction(&map));
+  printf("\n");
   return 0;
 }
