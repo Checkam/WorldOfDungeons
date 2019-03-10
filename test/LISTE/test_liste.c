f1cb6bb70a21662f90ff68f3c6bb68f37cba7766
Makcc <maxencedespres@gmail.com>
2019-03-10 14:31:21 +0100

    Modification du test_liste de Tibane (Erreur dans l'appel de valeur_elt)

--- test/LISTE/test_liste.c
+++ test/LISTE/test_liste.c
@@ -6,62 +6,56 @@
  * \version 0.1
 */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <liste.h>
 #include "test_liste.h"
+#include <liste.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    printf("----- Programme de Test de Liste Générique -----\n");
+int main() {
+  printf("----- Programme de Test de Liste Générique -----\n");
 
-    /* Valeurs */
-    /*float * a = malloc(sizeof(float)); *a = 1.1;
+  /* Valeurs */
+  /*float * a = malloc(sizeof(float)); *a = 1.1;
     float * b = malloc(sizeof(float)); *b = 2.2;
     float * c = malloc(sizeof(float)); *c = 3.3;*/
-    float a = 3, b = 4, c = 5;
+  float a = 3, b = 4, c = 5;
 
-    /* Déclaration des listes */
-    t_liste j1;
+  /* Déclaration des listes */
+  t_liste j1;
 
-    /* Création des listes */
-    printf("\t--> Création de la Liste Générique\n");
-    init_liste(&j1);
-    printf("\t\t-- OK\n");
+  /* Création des listes */
+  printf("\t--> Création de la Liste Générique\n");
+  init_liste(&j1);
+  printf("\t\t-- OK\n");
 
-    /* Remplissage des listes avec des valeurs quelconques */
-    printf("\t--> Remplissage de la Liste Générique\n");
-    en_tete(&j1);
-    ajout_droit(&j1,&a);
-    ajout_droit(&j1,&b);
-    ajout_droit(&j1,&c);
-    printf("\t\t-- OK\n");
+  /* Remplissage des listes avec des valeurs quelconques */
+  printf("\t--> Remplissage de la Liste Générique\n");
+  en_tete(&j1);
+  ajout_droit(&j1, &a);
+  ajout_droit(&j1, &b);
+  ajout_droit(&j1, &c);
+  printf("\t\t-- OK\n");
 
-    /* Affichage des listes */
-    printf("\t--> Affichage de la Liste Générique\n");
-    afficher_liste(&j1);
-    printf("\t\t-- OK\n");
+  /* Affichage des listes */
+  printf("\t--> Affichage de la Liste Générique\n");
+  afficher_liste(&j1);
+  printf("\t\t-- OK\n");
 
-    /* Destruction des listes */
-    printf("\t--> Destruction des Listes\n");
-    detruire_liste(&j1,(void *)mettre_null);
-    printf("\t\t-- OK\n");
+  /* Destruction des listes */
+  printf("\t--> Destruction des Listes\n");
+  detruire_liste(&j1, (void *)mettre_null);
+  printf("\t\t-- OK\n");
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
 
-void afficher_liste(t_liste * p)
-{
-    for(en_tete(p);!hors_liste(p);suivant(p))
-    {
-        float * val;
-        valeur_elt(p,(void **)&val,sizeof(float *));
-        printf("%.2f ", *val);
-    }
-    printf("\n");
+void afficher_liste(t_liste *p) {
+  for (en_tete(p); !hors_liste(p); suivant(p)) {
+    float *val;
+    valeur_elt(p, (void **)&val);
+    printf("%.2f ", *val);
+  }
+  printf("\n");
 }
 
-void mettre_null (float * f)
-{
-    f = NULL;
-}
+void mettre_null(float *f) { f = NULL; }
