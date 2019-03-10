2d43bcd92fed535c3db0737616505137c292f83d
Makcc <maxencedespres@gmail.com>
2019-03-10 14:00:15 +0100

    Ajout des prototypes des fonctions de block dans block.h + ifndef

--- include/block.h
+++ include/block.h
@@ -1,35 +1,26 @@
-#include <SDL2/SDL.h>
+#ifndef __BLOCK_H__
+#define __BLOCK_H__
+
+#include <couleurs.h>
+
 #define NB_BLOCK 10
 
+typedef enum { AIR, HERBE, TERRE, EAU, SABLE, FEUILLE, BOIS, ROCHE, NEIGE, GLACE, DIAMAND } materiaux_t;
 
-typedef enum {
-	AIR,
-	HERBE,
-	TERRE,
-	EAU,
-	SABLE,
-	FEUILLE,
-	BOIS,
-	ROCHE,
-	NEIGE,
-	GLACE,
-	DIAMAND
-} materiaux_t;
+typedef struct block_type_s {
+  materiaux_t materiau;
+  char *nom;
+  int resistance;
 
-typedef struct block_type_s{
-    materiaux_t materiau;
-    char * nom;
-	//int resistance;
-	SDL_Texture * texture;
-}block_type_t;
+  char *texture_sdl;
+  char *texture_term;
+} block_type_t;
 
-typedef struct block_type2_s{
-    materiaux_t materiau;
-    char * nom;
-	//int resistance;
-	char * texture;
-}block_type2_t;
+typedef struct block_s {
+  int id, x, y;
+} block_t;
 
-typedef struct block_s{
-    int id,x,y;
-}block_t;
+#endif
+
+char *BLOCK_GetTexture_sdl(int id);
+char *BLOCK_GetTexture_term(int id);
