eadca81dad764937bf0654ed0d8142c2f81bba4e
Checkam <maxencedespres@gmail.com>
2019-03-11 18:10:17 +0100

    Création du module biome

---
+++ include/biome.h
@@ -0,0 +1,20 @@
+
+#ifndef __BIOME_H__
+#define __BIOME_H__
+
+#define W_BIOME 200
+#define NB_BIOME 5
+
+typedef enum {
+       FORET,
+       PRAIRIES,
+       TAIGA,
+       TOUNDRA,
+       DESERTS
+} biome_t;
+
+
+biome_t biome;
+
+
+#endif
