d5a143d55831f5b49de85e6923972538a640a060
Checkam <maxencedespres@gmail.com>
2019-03-11 19:31:01 +0100

    Modification de la taille d'affichage : - 25 * 25

--- include/commun.h
+++ include/commun.h
@@ -1,12 +1,10 @@
 #define N 800       /* Number of iterations */
 #define MAX 60     /* Heigth of screen */
 #define width_window 1200
-#define widthBrick 50
+#define widthBrick 25
 #define height_window 700
-#define heightBrick 50
+#define heightBrick 25
 #define SIZE (width_window / widthBrick)    /* Width of screen */
 #define MAX_SCREEN (height_window / heightBrick)
 
 int SEED;
-
-
