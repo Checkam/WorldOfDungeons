27f9c88aa4a13e197b7570108f6f2d5bcfbc1cc0
Checkam <maxencedespres@gmail.com>
2019-03-12 15:24:37 +0100

    Modification du module structure block

--- src/structure_block.c
+++ src/structure_block.c
@@ -15,40 +15,72 @@
 #include <string.h>
 #include <structure_block.h>
 
+int STRUCT_GetWidth(t_struct_block *struct_block, t_struct_block_type type) {
+  for (int i = 0; i < NB_STRUCT_BLOCK; i++) {
+    if (struct_block[i].type == type)
+      return struct_block[i].largeur;
+  }
+  return 0;
+}
+
+char *STRUCT_GetPath(t_struct_block *struct_block, t_struct_block_type type) {
+  for (int i = 0; i < NB_STRUCT_BLOCK; i++) {
+    if (struct_block[i].type == type)
+      return struct_block[i].path;
+  }
+  return 0;
+}
+
 void STRUCT_generation(int x, t_block map[MAX], int y) {
   static int struct_spawn = 0;
   static int new_struct = 0;
   static t_struct_block_type type_spawn;
+  static t_struct_block_type type_can_spawn[NB_STRUCT_BLOCK];
+  int nb_can_spawn = 0;
+  int pourcent = 0;
+  int struct_random = 0;
 
-  t_struct_block struct_block[NB_STRUCT_BLOCK] = {{ARBRE_TAIGA, 7, "structure/arbre/arbre_taiga", 30, (biome == TAIGA)},
-                                                  {ARBRE_FORET, 7, "structure/arbre/arbre_foret", 30, (biome == FORET)},
-                                                  {BUISSON, 4, "structure/arbre/buisson", 30, (biome == FORET)},
-                                                  {CACTUS, 3, "structure/arbre/cactus", 30, (biome == DESERTS)}};
+  t_struct_block struct_block[NB_STRUCT_BLOCK] = {{ARBRE_TAIGA, 7, "structure/arbre_taiga", 30, (biome == TAIGA)},
+                                                  {ARBRE_FORET, 7, "structure/arbre_foret", 0, (biome == FORET)},
+                                                  {GRAND_ARBRE_FORET, 7, "structure/arbre_foret2", 100, (biome == FORET)},
+                                                  {BUISSON, 8, "structure/buisson", 60, (biome == FORET)},
+                                                  {CACTUS, 3, "structure/cactus", 30, (biome == DESERTS)}};
+
+  if (struct_spawn) {
+    char line[50];
+    FILE *file = NULL;
+    file = fopen(STRUCT_GetPath(struct_block, type_spawn), "r");
+
+    if (file != NULL) {
+      for (int j = 0; j < struct_spawn; j++) {
+        for (int l = 0; l < 50; l++)
+          line[l] = 0;
+        fgets(line, 50, file);
+      }
+
+      for (int j = 0; j < strlen(line) && (j + y < MAX - 1); j++)
+        map[y + j].id = line[j] - '0';
+
+      fclose(file);
+    }
+    struct_spawn--;
+  }
 
   for (int i = 0; i < NB_STRUCT_BLOCK; i++)
-    if (!struct_spawn && !new_struct && struct_block[i].condition && y > HAUTEUR_EAU) {
-      struct_spawn = struct_block[i].largeur;
-      new_struct = (int)((double)x * perlin2d(x, MAX, FREQ, DEPTH) * (double)W_BIOME + 1) % DIST_MAX_TREE;
-      type_spawn = struct_block[i].type;
-    } else if (struct_spawn && type_spawn == struct_block[i].type) {
-      char line[50];
-      FILE *file = NULL;
-      file = fopen(struct_block[i].path, "r");
-
-      if (file != NULL) {
-        for (int j = 0; j < struct_spawn; j++) {
-          for (int l = 0; l < 50; l++)
-            line[l] = 0;
-          fgets(line, 50, file);
-        }
-
-        for (int j = 0; j < strlen(line) && (j + y < MAX - 1); j++)
-          map[y + j].id = line[j] - '0';
-
-        fclose(file);
+    if (nb_can_spawn < NB_STRUCT_BLOCK && !new_struct && struct_block[i].condition && y > HAUTEUR_EAU) {
+      pourcent = ((int)((double)x * perlin2d(x, MAX, FREQ, DEPTH) * 1232321) % 100) + 1;
+      if (pourcent < struct_block[i].pourcent_spawn) {
+        type_can_spawn[nb_can_spawn] = struct_block[i].type;
+        nb_can_spawn++;
       }
-      struct_spawn--;
     } else if (!struct_spawn && new_struct > 0 && type_spawn == struct_block[i].type) {
       new_struct--;
     }
+
+  if (!struct_spawn && !new_struct && nb_can_spawn > 0) {
+    new_struct = (int)((double)x * perlin2d(x, MAX, FREQ, DEPTH) * (double)W_BIOME + 1) % DIST_MAX_TREE;
+    struct_random = (int)((double)x * perlin2d(x, MAX, FREQ, DEPTH) * (double)W_BIOME + 1) % nb_can_spawn;
+    type_spawn = type_can_spawn[struct_random];
+    struct_spawn = STRUCT_GetWidth(struct_block, type_spawn);
+  }
 }
