016c8b5763c857c904d9f65f5ff8466ee661ca9c
CPietJa <tibane.galbrun@gmail.com>
2019-03-13 13:35:20 +0100

    Création du module entite

---
+++ include/entite.h
@@ -0,0 +1,11 @@
+#ifndef __ENTITE_H__
+#define __ENTITE_H__
+/**
+ * \file entite.h
+ * \brief Module contenant les primitives de gestion des entités.
+ * \author GALBRUN Tibane
+ * \date 13/03/2019
+ * \version 0.1
+*/
+
+#endif
---
+++ src/entite.c
@@ -0,0 +1,7 @@
+/**
+ * \file entite.c
+ * \brief Module servant à gérer les entités.
+ * \author GALBRUN Tibane
+ * \date 13/03/2019
+ * \version 0.1
+*/
