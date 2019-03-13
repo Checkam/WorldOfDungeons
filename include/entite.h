9997cd9a61c7f97408d1b7ff1816462d507b11d4
CPietJa <tibane.galbrun@gmail.com>
2019-03-13 16:25:48 +0100

    Ajout des fonctions de création d'une entité

--- include/entite.h
+++ include/entite.h
@@ -8,4 +8,48 @@
  * \version 0.1
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <erreur.h>
+
+typedef enum{
+    JOUEUR,
+    ZOMBIE,
+    BOSS
+}t_entite_type;
+
+/**
+ * \struct t_hitbox
+ * \brief Contient la position et la taille de la hitbox.
+*/
+typedef struct s_hitbox
+{
+    int x,y;
+    int l,L;
+}t_hitbox;
+
+/**
+ * \struct t_entite
+ * \brief Contient une entite et ses paramètres.
+*/
+typedef struct s_entite
+{
+    int id;
+    char * name;
+    long int xp;
+    int mana, pv, faim;
+    int mana_max, pv_max, faim_max;
+    SDL_Texture * texture;
+    t_hitbox hitbox;
+    // Structure inventaire à rajouter
+}t_entite;
+
+/* Créer une entité avec des paramètres par défaut */
+t_entite * creer_entite_defaut (char * name, t_hitbox hitbox, SDL_Texture * texture, t_entite_type type);
+/* Créer une entité */
+t_entite * creer_entite (char * name, int mana, int mana_max, int pv, int pv_max, SDL_Texture * texture, t_hitbox hitbox);
+/* Créer une hitbox */
+t_hitbox creer_hitbox (int x, int y, int largeur, int longueur);
+
 #endif
--- src/entite.c
+++ src/entite.c
@@ -5,3 +5,58 @@
  * \date 13/03/2019
  * \version 0.1
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <entite.h>
+#include <erreur.h>
+
+/**
+ * \fn t_entite * creer_entite_defaut (char * name, t_hitbox hitbox, SDL_Texture * texture, t_entite_type type)
+ * \brief Créer une entité avec des paramètres par défaut.
+ * \param name Le nom de l'entité.
+ * \param hitbox La hitbox de l'entité.
+ * \param texture La texture de l'entité.
+ * \param type Le type de l'entité.
+ * \return Un pointeur sur l'entité créée.
+*/
+t_entite * creer_entite_defaut (char * name, t_hitbox hitbox, SDL_Texture * texture, t_entite_type type)
+{
+    if (!hitbox || !texture) return NULL;
+    switch (type){
+        case JOUEUR : return creer_entite_defaut((name) ? name : "PLAYER", 20, 20, 10, 10, texture, hitbox);
+        case ZOMBIE : return creer_entite_defaut((name) ? name : "ZOMBIE", 0, 0, 10, 10, texture, hitbox);
+        case BOSS : return creer_entite_defaut((name) ? name : "BOSS", 50, 50, 30, 30, texture, hitbox);
+    }
+    return NULL;
+}
+
+t_entite * creer_entite (char * name, int mana, int mana_max, int pv, int pv_max, SDL_Texture * texture, t_hitbox hitbox)
+{
+    if (!hitbox || !texture || !name) return NULL;
+
+    t_entite * entite = malloc(sizeof(t_entite));
+    entite->id = sizeof(*texture);
+    entite->name = name;
+    entite->xp = 0;
+    entite->faim = entite->faim_max = 10;
+    entite->mana = mana;
+    entite->mana_max = mana_max;
+    entite->pv = pv;
+    entite->pv_max = pv_max;
+    entite->texture = texture;
+    entite->hitbox = hitbox;
+
+    return entite;
+}
+
+t_hitbox creer_hitbox (int x, int y, int largeur, int longueur)
+{
+    t_hitbox hit;
+    hit.x = x;
+    hit.y = y;
+    hit.l = largeur;
+    hiy.L = longueur;
+    return hit;
+}
