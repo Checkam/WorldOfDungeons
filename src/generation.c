f84f80c54b9cbdd73d1af94821e83db006291cdb
Checkam <maxencedespres@gmail.com>
2019-03-15 08:52:10 +0100

    MAJ Fonction génération avec le nouveau module de biome

--- src/generation.c
+++ src/generation.c
@@ -53,62 +53,64 @@ int gen_col(t_block **tab, int x) {
   int taille_max = perlin2d(x, MAX, FREQ, DEPTH) * MAX;
   *tab = malloc(sizeof(t_block) * MAX);
   init_map(*tab);
-  changeBiome(x, taille_max);
+  biome = BIOME_change_type(x, taille_max);
 
   STRUCT_generation(x, *tab, taille_max);
+  t_biome *b = BIOME_rechercheParType(biome);
 
   /* génération eau */
-  for (j = 0; taille_max + j < HAUTEUR_EAU; j++) {
-    if (j + taille_max == HAUTEUR_EAU - 1 && (biome == TOUNDRA || biome == TAIGA)) {
-      (*tab)[taille_max + j].id = GLACE;
-      (*tab)[taille_max + j].y = taille_max + j;
-      (*tab)[taille_max + j].x = x;
-    } else if ((*tab)[taille_max + j].id == AIR) {
-      (*tab)[taille_max + j].id = EAU;
-      (*tab)[taille_max + j].y = taille_max + j;
-      (*tab)[taille_max + j].x = x;
+  // for (j = 0; taille_max < HAUTEUR_EAU; j++) {
+  //   if (j + taille_max == HAUTEUR_EAU - 1 && (biome == TOUNDRA || biome == TAIGA)) {
+  //     (*tab)[taille_max + j].id = GLACE;
+  //     (*tab)[taille_max + j].y = taille_max + j;
+  //     (*tab)[taille_max + j].x = x;
+  //   } else if ((*tab)[taille_max + j].id == AIR) {
+  //     (*tab)[taille_max + j].id = EAU;
+  //     (*tab)[taille_max + j].y = taille_max + j;
+  //     (*tab)[taille_max + j].x = x;
+  //   }
+  // }
+  for (int i = 0, layer = 0; i < b->nb_layers; i++) {
+    for (int j = layer, k = 0; k < b->layers[i].nb_couche && layer < taille_max; j++, layer++, k++) {
+      (*tab)[taille_max - j].id = b->layers[i].block_type;
+      (*tab)[taille_max - j].y = taille_max - j;
+      (*tab)[taille_max - j].x = x;
     }
+    printf("%d\n", layer);
   }
 
   /* génération herbe + sable + terre */
-  for (j = 1; j <= HAUTEUR_SURFACE; j++) {
-    if (((*tab)[taille_max].id == EAU || (*tab)[taille_max].id == GLACE) && j == 1) {
-      (*tab)[taille_max - j].id = SABLE;
-      (*tab)[taille_max - j].y = taille_max - j;
-      (*tab)[taille_max - j].x = x;
-    } else if (j == 1) {
-      if (biome == PRAIRIES || biome == FORET) {
-        (*tab)[taille_max - j].id = HERBE;
-        (*tab)[taille_max - j].y = taille_max - j;
-        (*tab)[taille_max - j].x = x;
-      } else if (biome == TOUNDRA || biome == TAIGA) {
-        (*tab)[taille_max - j].id = NEIGE;
-        (*tab)[taille_max - j].y = taille_max - j;
-        (*tab)[taille_max - j].x = x;
-      } else if (biome == DESERTS) {
-        (*tab)[taille_max - j].id = SABLE;
-        (*tab)[taille_max - j].y = taille_max - j;
-        (*tab)[taille_max - j].x = x;
-      }
-    } else {
-      (*tab)[taille_max - j].id = TERRE;
-      (*tab)[taille_max - j].y = taille_max - j;
-      (*tab)[taille_max - j].x = x;
-    }
-  }
-
-  (*tab)[taille_max - HAUTEUR_SURFACE].id = GRAVIER;
+  // for (j = 1; j <= HAUTEUR_SURFACE; j++) {
+  //   if (((*tab)[taille_max].id == EAU || (*tab)[taille_max].id == GLACE) && j == 1) {
+  //     (*tab)[taille_max - j].id = SABLE;
+  //     (*tab)[taille_max - j].y = taille_max - j;
+  //     (*tab)[taille_max - j].x = x;
+  //   } else if (j == 1) {
+  //     if (biome == PRAIRIES || biome == FORET) {
+  //       (*tab)[taille_max - j].id = HERBE;
+  //       (*tab)[taille_max - j].y = taille_max - j;
+  //       (*tab)[taille_max - j].x = x;
+  //     } else if (biome == TOUNDRA || biome == TAIGA) {
+  //       (*tab)[taille_max - j].id = NEIGE;
+  //       (*tab)[taille_max - j].y = taille_max - j;
+  //       (*tab)[taille_max - j].x = x;
+  //     } else if (biome == DESERTS) {
+  //       (*tab)[taille_max - j].id = SABLE;
+  //       (*tab)[taille_max - j].y = taille_max - j;
+  //       (*tab)[taille_max - j].x = x;
+  //     }
+  //   } else {
+  //     (*tab)[taille_max - j].id = TERRE;
+  //     (*tab)[taille_max - j].y = taille_max - j;
+  //     (*tab)[taille_max - j].x = x;
+  //   }
+  // }
 
   /* Génération profondeur */
-  for (j = 0; j < taille_max - HAUTEUR_SURFACE; j++) {
+  for (j = 0; j <= taille_max - HAUTEUR_SURFACE; j++) {
     rnd = perlin2d(x, j, FREQ, DEPTH) * MAX;
-
     if (taille_max - j > PROFONDEUR_GROTTE && j >= BEDROCK && rnd >= (MAX / 2 - SIZE_GROTTE) && rnd <= (MAX / 2 + SIZE_GROTTE)) { /* grotte */
       (*tab)[j].id = AIR;
-      (*tab)[j].y = j;
-      (*tab)[j].x = x;
-    } else {
-      (*tab)[j].id = ROCHE;
       (*tab)[j].y = j;
       (*tab)[j].x = x;
     }
