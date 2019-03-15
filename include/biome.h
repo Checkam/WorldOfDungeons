623cd60ea7437dfe97e3f8124d97c88dd5a16eb8
Checkam <maxencedespres@gmail.com>
2019-03-15 08:51:28 +0100

    Premiere version fonctionnel du module biome

--- include/biome.h
+++ include/biome.h
@@ -13,43 +13,44 @@
 #include <liste.h>
 
 #define W_BIOME 200
-#define NB_BIOME 5
+#define NB_BIOME 7
 
 /**
  * \enum t_biome_type
  * \brief Enumeration de type de biome
 **/
 typedef enum {
-       FORET= 0,
+       FORET = 0,
        PRAIRIES,
        TAIGA,
        TOUNDRA,
-       DESERTS
+       DESERTS,
+       HELL,
+       HEAVEN
 } t_biome_type;
 
 
 t_biome_type biome;
 
 typedef struct {
-  t_block_type block_type;
-  int hauteur_max,hauteur_min;
+  t_materiaux block_type;
+  int nb_couche;
 } t_layer;
 
 typedef struct {
   t_biome_type type;
-  t_liste *layers; //liste de layer
+  t_layer *layers; //liste de layer
   int nb_layers;
 } t_biome;
 
 
-
-
-
-
-
 #endif
 
-void changeBiome(int x, int y);
+t_biome_type BIOME_change_type(int x, int y);
 void BIOME_init();
 t_biome *BIOME_CreateBiome(t_biome_type type);
-void BIOME_AddLayer(t_biome *b, t_block_type block_type, int min, int max);
+void BIOME_AddLayer(t_biome *b, t_materiaux block_type, int);
+t_biome *BIOME_rechercheParType(t_biome_type type);
+void BIOME_Quit();
+void BIOME_DestroyLayer(t_layer *layer);
+void BIOME_Destoy(t_biome *b);
--- src/biome.c
+++ src/biome.c
@@ -14,49 +14,105 @@
  * \return Ne retourne rien
 **/
 
-static t_liste biomes;
+t_liste biomes;
 
-void changeBiome(int x, int y) {
+t_biome_type BIOME_change_type(int x, int y) {
   if (x % W_BIOME == SEED % W_BIOME) {
     // Génere un chiffre entre 0 et le nombre de biome qui existe
-    biome = (int)((double)x * perlin2d(x - 1, y, FREQ, DEPTH) * SEED + 1) % NB_BIOME;
+    return (t_biome_type)((int)((double)x * perlin2d(x - 1, y, FREQ, DEPTH) * SEED + 1) % NB_BIOME);
   }
+  return biome;
 }
 
 void BIOME_init() {
   init_liste(&biomes);
+
   t_biome *b = BIOME_CreateBiome(FORET);
+  BIOME_AddLayer(b, HERBE, 1);
+  BIOME_AddLayer(b, TERRE, 4);
+  BIOME_AddLayer(b, ROCHE, MAX);
+
+  b = BIOME_CreateBiome(PRAIRIES);
+  BIOME_AddLayer(b, HERBE, 1);
+  BIOME_AddLayer(b, TERRE, 4);
+  BIOME_AddLayer(b, ROCHE, MAX);
+
   b = BIOME_CreateBiome(TAIGA);
+  BIOME_AddLayer(b, NEIGE, 1);
+  BIOME_AddLayer(b, TERRE, 4);
+  BIOME_AddLayer(b, ROCHE, MAX);
+
   b = BIOME_CreateBiome(TOUNDRA);
-  BIOME_AddLayer(b, NEIGE, 10, 100);
+  BIOME_AddLayer(b, NEIGE, 1);
+  BIOME_AddLayer(b, TERRE, 4);
+  BIOME_AddLayer(b, ROCHE, MAX);
+
+  b = BIOME_CreateBiome(DESERTS);
+  BIOME_AddLayer(b, SABLE, 1);
+  BIOME_AddLayer(b, TERRE, 4);
+  BIOME_AddLayer(b, ROCHE, MAX);
+
+  b = BIOME_CreateBiome(HELL);
+  BIOME_AddLayer(b, ROCHE, 1);
+  BIOME_AddLayer(b, ROCHE, 4);
+  BIOME_AddLayer(b, ROCHE, MAX);
+
+  b = BIOME_CreateBiome(HEAVEN);
+  BIOME_AddLayer(b, GLACE, 1);
+  BIOME_AddLayer(b, FEUILLE, 1);
+  BIOME_AddLayer(b, TERRE, 1);
+  BIOME_AddLayer(b, HERBE, 1);
+  BIOME_AddLayer(b, TERRE, 1);
+  BIOME_AddLayer(b, GLACE, MAX);
 }
 
 t_biome *BIOME_rechercheParType(t_biome_type type) {
-  t_biome *b = malloc(sizeof(t_biome));
+  t_biome *b;
   for (en_tete(&biomes); !hors_liste(&biomes); suivant(&biomes)) {
     valeur_elt(&biomes, (void **)&b);
     if (b->type == type)
       return b;
   }
-  free(b);
   return NULL;
 }
+
+void BIOME_afficher_type() {}
 
 t_biome *BIOME_CreateBiome(t_biome_type type) {
   t_biome *b = malloc(sizeof(t_biome));
   b->type = type;
-  b->layers = malloc(sizeof(t_liste));
-  init_liste(b->layers);
+  b->layers = malloc(sizeof(t_layer) * MAX);
   b->nb_layers = 0;
   ajout_droit(&biomes, b);
   return b;
 }
 
-void BIOME_AddLayer(t_biome *b, t_materiaux block_type, int min, int max) {
-  t_layer *layer = malloc(sizeof(t_layer));
-  layer->hauteur_max = max;
-  layer->hauteur_min = min;
-  layer->block_type = block_type;
-  ajout_droit(b->layers, layer);
+void BIOME_AddLayer(t_biome *b, t_materiaux block_type, int nb_couche) {
+  b->layers[b->nb_layers].nb_couche = nb_couche;
+  b->layers[b->nb_layers].block_type = block_type;
+  b->nb_layers++;
   modif_elt(&biomes, b);
+}
+
+void BIOME_Quit() {
+  t_biome *b;
+  for (en_queue(&biomes); !hors_liste(&biomes) || !liste_vide(&biomes); en_queue(&biomes)) {
+    valeur_elt(&biomes, (void **)&b);
+    BIOME_Destoy(b);
+    oter_elt(&biomes, free);
+  }
+  detruire_liste(&biomes, free);
+}
+
+void BIOME_DestroyLayer(t_layer *layer) {
+  if (layer != NULL) {
+    free(layer);
+  }
+}
+void BIOME_Destoy(t_biome *b) {
+  if (b != NULL) {
+    if (b->layers != NULL) {
+      free(b->layers);
+    }
+  }
 }
