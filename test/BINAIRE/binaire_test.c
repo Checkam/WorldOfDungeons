232e2f0e113b6aed5b381c2366a453d6e71f9e1a
CPietJa <tibane.galbrun@gmail.com>
2019-03-25 15:38:27 +0100

    Correctif inclusion

--- test/BINAIRE/binaire_test.c
+++ test/BINAIRE/binaire_test.c
@@ -4,19 +4,12 @@
 #include <erreur.h>
 #include <binaire.h>
 #include <assert.h>
-#include <memory.h>
-#include "binaire_test.h"
 
 int main(int argc, char **argv, char ** env) {
     printf("-------- Programme de Test du Module BINAIRE --------\n");
 
     pwd_init(argv[0],getenv("PWD"));
-    printf("%d\n",add(1,2));
+    
     pwd_quit();
     return OK;
-}
-
-static int add (int a, int b)
-{
-    return a + b;
 }
--- test/BINAIRE/binaire_test.h
+++
@@ -1,1 +0,0 @@
-static int add (int, int);
