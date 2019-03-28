c9f6f28c488cb9c4d59a29a3271225b551a481fe
jashbin <jasmin.galbrun@gmail.com>
2019-03-28 16:31:29 +0100

    Ajout mob donjon

--- include/donjon.h
+++ include/donjon.h
@@ -12,6 +12,7 @@
 #include <liste.h>
 #include <SDL2/SDL.h>
 #include <entite.h>
+#include <stdbool.h>
 
 typedef enum e_type_salle_donjon{
     DONJON_DEPART,
@@ -35,7 +36,7 @@ typedef struct s_salle_donjon{
     t_type_salle_donjon type;
     t_salle_difficulte difficulte;
 
-    int salle_completed;
+    bool completed;
     t_liste * mob;
 }t_salle_donjon;
 
@@ -52,7 +53,7 @@ t_erreur donjon_afficher_Term(t_donjon * donjon, t_entite * joueur);
 t_erreur donjon_afficher_SDL(SDL_Renderer * renderer, t_donjon * donjon, t_entite * joueur);
 
 /* Primitive de gestion d'un donjon */
-
+t_erreur donjon_gestion(SDL_Renderer * renderer, t_donjon * donjon, t_entite * joueur);
 
 /* Primitive de destruction d'un donjon */
 t_erreur donjon_detruire(t_donjon **donjon);
