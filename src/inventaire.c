95033bcf564b00efb1bb93295e7d919584567f7d
VoxoR7 <mvaudeleau@laposte.net>
2019-04-02 22:41:30 +0200

    suite module inventaire et commentaire

--- src/inventaire.c
+++ src/inventaire.c
@@ -51,24 +51,41 @@ void ajout_item_dans_inventaire( t_inventaire *inventaire, t_liste *listeItem ) 
 
     en_tete( listeItem );
 
-    uint16_t placeLibre;
-    t_liste_item *ptritem;
+    uint16_t placeLibre, i;
+    t_liste_item *ptritem, *t = NULL;
 
     #ifdef DEBUG 
         assert( listeItem != NULL && inventaire != NULL );
     #endif
 
+    printf("\n");
+
+    /* pour tous les éléments de la liste */
     while ( !hors_liste( listeItem ) ) {
 
         valeur_elt( listeItem, (void **)&ptritem );
 
-        printf("\n item : %d\n", ptritem->item);
+        printf("Traitement de l'element %p ...\n", ptritem);
 
-        placeLibre = 0;
+        valeur_elt( listeItem, ( void **)&t);
+        printf("\tla valeur suivante sera : %p\n", t);
+        t = NULL;
 
-        while ( ((( inventaire->inventaire) + placeLibre)->item) != (tabItem + ( ptritem->item )) && (( inventaire->inventaire) + placeLibre)->stack < (tabItem + ( ptritem->item ))->stack && placeLibre < inventaire->nbItemMax )
-            placeLibre++;
+        i = 0;
+        placeLibre = inventaire->nbItemMax;
 
+        /* recherche d'une place d'inventaire deja occupé par l'item que l'on souhaite mettre && que cette case ne soit pas rempli */
+        while ( i < inventaire->nbItemMax ) {
+
+            if ( ((( inventaire->inventaire) + i)->item) == (tabItem + ( ptritem->item )) && (( inventaire->inventaire) + i)->stack < (tabItem + ( ptritem->item ))->stack ) {
+
+                placeLibre = i;
+                i = inventaire->nbItemMax;
+            }
+            i++;
+        }
+        
+        /* si aucune place trouvé alors recherche d'un case d'inventaire libre */
         if ( placeLibre >= inventaire->nbItemMax ) {
 
             placeLibre = 0;
@@ -77,26 +94,44 @@ void ajout_item_dans_inventaire( t_inventaire *inventaire, t_liste *listeItem ) 
                 placeLibre++;
         }
 
+        /* si une place a été trouvé alors ajout de l'item dans l'inventaire */
         if ( placeLibre < inventaire->nbItemMax ) {
 
+            /* cas 1 --> trop d'objet a casé pour que ca passe dans une seule case */
             if ( ((inventaire->inventaire) + placeLibre)->stack + ptritem->nbDrop > (tabItem + ( ptritem->item ))->stack ) {
 
                 ( (inventaire->inventaire) + placeLibre )->stack = (tabItem + ( ptritem->item ))->stack;
+                printf("\tnb item avant : %d\n", ptritem->nbDrop);
                 ptritem->nbDrop -= (tabItem + ( ptritem->item ))->stack;
+                printf("\tnb item apres : %d\n", ptritem->nbDrop);
 
+                /*printf("\tajout de l'element %p ...\n", ptritem);*/
                 ajout_droit(listeItem, (void *) ptritem);
+                /*valeur_elt( listeItem, ( void *)t);
+                printf("\tpositionnement sur l'element %p\n", t);
+                t = NULL;*/
                 precedent(listeItem);
+                /*valeur_elt( listeItem, ( void *)t);
+                printf("\tsecond positionnement sur l'element %p\n", t);
+                t = NULL;*/
 
                 inventaire->nbItem++;
+
+            /* cas 2 --> tous passe dans une seule case */
             } else
                 ( (inventaire->inventaire) + placeLibre )->stack += ptritem->nbDrop;
 
             (( (inventaire->inventaire) + placeLibre )->item) = (tabItem + ( ptritem->item ));
             ( (inventaire->inventaire) + placeLibre )->durabilite = (tabItem + ( ptritem->item ))->durabilite;
 
+            valeur_elt( listeItem, ( void **)&t);
+            printf(" ... suppression de l'element %p\n", t);
+            t = NULL;
             oter_elt( listeItem, free);
+
+            en_tete(listeItem);
+        } else
             suivant(listeItem);
-        }
     }
 }
 
