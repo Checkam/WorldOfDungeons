000e5678f668ce957a274acdd31e4e4a5a06963f
Checkam <maxencedespres@gmail.com>
2019-04-05 16:24:52 +0200

    Ajout des fonctions STRUCT_Spawn, STRUCT_SetCol

--- src/structure_block.c
+++ src/structure_block.c
@@ -9,11 +9,13 @@
 #include <block.h>
 #include <commun.h>
 #include <generation.h>
+#include <map.h>
 #include <perlin.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <structure_block.h>
+#include <touches.h>
 
 t_struct_block struct_block[NB_STRUCT_BLOCK] = {{ARBRE_TAIGA, 7, "structure/arbre_taiga", 30, TAIGA},
                                                 {ARBRE_FORET, 7, "structure/arbre_foret", 80, FORET},
@@ -21,7 +23,7 @@ t_struct_block struct_block[NB_STRUCT_BLOCK] = {{ARBRE_TAIGA, 7, "structure/arbr
                                                 {BUISSON, 5, "structure/buisson", 60, FORET},
                                                 {CACTUS, 3, "structure/cactus", 30, DESERTS}};
 
-int STRUCT_GetWidth(t_struct_block *struct_block, t_struct_block_type type) {
+int STRUCT_GetWidth(t_struct_block_type type) {
   for (int i = 0; i < NB_STRUCT_BLOCK; i++) {
     if (struct_block[i].type == type)
       return struct_block[i].largeur;
@@ -29,7 +31,7 @@ int STRUCT_GetWidth(t_struct_block *struct_block, t_struct_block_type type) {
   return 0;
 }
 
-char *STRUCT_GetPath(t_struct_block *struct_block, t_struct_block_type type) {
+char *STRUCT_GetPath(t_struct_block_type type) {
   for (int i = 0; i < NB_STRUCT_BLOCK; i++) {
     if (struct_block[i].type == type)
       return struct_block[i].path;
@@ -39,6 +41,41 @@ char *STRUCT_GetPath(t_struct_block *struct_block, t_struct_block_type type) {
 
 int STRUCT_CanSpawn(int x, int i) { return 0; }
 
+int STRUCT_Spawn(int x, int y, t_struct_block_type type_spawn, t_map *map) {
+  t_block *tab;
+  x -= STRUCT_GetWidth(type_spawn) / 2;
+  for (int i = 0; i <= STRUCT_GetWidth(type_spawn); i++) {
+    tab = MAP_GetColX(map, x + i);
+    STRUCT_SetCol(i, y, type_spawn, tab);
+  }
+  return 0;
+}
+
+int STRUCT_SetCol(int x_struct, int y, t_struct_block_type type_spawn, t_block *tab) {
+  if (!tab)
+    return -1;
+
+  char line[50];
+  FILE *file = NULL;
+  file = fopen(STRUCT_GetPath(type_spawn), "r");
+
+  if (file != NULL) {
+    for (int j = 0; j < x_struct; j++) {
+      for (int l = 0; l < 50; l++)
+        line[l] = 0;
+      fgets(line, 50, file);
+    }
+
+    for (int j = 0; j < strlen(line) && (j + y < MAX - 1); j++) {
+      if (line[j] - '0' >= 0)
+        tab[y + j].id = line[j] - '0';
+    }
+
+    fclose(file);
+  }
+  return 0;
+}
+
 /**
  * \fn void STRUCT_generation(int x, t_block map[MAX], int y)
  * \brief Permet de generer les structures block
@@ -46,9 +83,9 @@ int STRUCT_CanSpawn(int x, int i) { return 0; }
  * \param map le tableau de block ou ajouter la structure
  * \param y hauteur du tableau actuel
 **/
-void STRUCT_generation(int x, int y, int dir, t_block map[MAX]) {
+void STRUCT_generation(int x, int y, int dir, t_block tab[MAX]) {
   y++;
-  static int struct_spawn = 0, new_struct = 0, struct_spawn_dir1, struct_spawn_dir2, new_struct_dir1, new_struct_dir2;
+  static int struct_spawn = 0, new_struct = 0, struct_spawn_dir1, new_struct_dir1;
   static t_struct_block_type type_spawn;
   static t_struct_block_type type_can_spawn[NB_STRUCT_BLOCK];
   static int last_dir;
@@ -63,23 +100,14 @@ void STRUCT_generation(int x, int y, int dir, t_block map[MAX]) {
     last_dir = dir;
   }
 
+  if (dir == DROITE) {
+    x += 3;
+  } else if (dir == GAUCHE) {
+    x -= 3;
+  }
+
   if (struct_spawn) { // Permet de prendre la ligne du block a test
-    char line[50];
-    FILE *file = NULL;
-    file = fopen(STRUCT_GetPath(struct_block, type_spawn), "r");
-
-    if (file != NULL) {
-      for (int j = 0; j < struct_spawn; j++) {
-        for (int l = 0; l < 50; l++)
-          line[l] = 0;
-        fgets(line, 50, file);
-      }
-
-      for (int j = 0; j < strlen(line) && (j + y < MAX - 1); j++)
-        map[y + j].id = line[j] - '0';
-
-      fclose(file);
-    }
+    STRUCT_SetCol(struct_spawn, y, type_spawn, tab);
     struct_spawn--;
   } else if (!struct_spawn && new_struct > 0) { //Attente avant une nouvelle ligne
     new_struct--;
@@ -96,6 +124,6 @@ void STRUCT_generation(int x, int y, int dir, t_block map[MAX]) {
     new_struct = (int)((double)x * perlin2d(x, MAX, FREQ, DEPTH) * (double)W_BIOME + 1) % DIST_MAX_STRUCT;
     struct_random = (int)((double)x * perlin2d(x, MAX, FREQ, DEPTH) * (double)W_BIOME + 1) % nb_can_spawn;
     type_spawn = type_can_spawn[struct_random];
-    struct_spawn = STRUCT_GetWidth(struct_block, type_spawn);
+    struct_spawn = STRUCT_GetWidth(type_spawn);
   }
 }
